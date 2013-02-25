a995a1a6c59f25731d9d821435962d48a2dd2898
Quinn Thorsnes <quinn.frogman@gmail.com>
2013-02-24 22:46:41 -0800

    Adding my (Quinn's) Silly AI
    I didn't want to have my silly AI have the same name as the original and
    wanted it it recognizable as mine.

---
+++ sillyQ.py
@@ -0,0 +1,28 @@
+import AIClass
+import copy
+import random
+
+class sampleAI(AIClass.AI):
+	prevBoard = ""
+	def makeMove(self, board):
+		if(self.prevBoard == ""):
+			self.dropMarker(board,random.randint(0,6))
+		else:
+			lastMove = self.findLastMove(board)
+			if(lastMove == 0 or lastMove == -1):
+				self.dropMarker(board, 0)
+			else:
+				self.dropMarker(board, random.randin(0,6))
+		self.prevBoard = copy.deepcopy(board)
+	def findLastMove(self, board):
+		self.printBoard(board)
+		self.printBoard(self.prevBoard)
+		i = 0
+		while(i<6):
+			j = 0
+			while(j<7):
+				if(board[j][i] != self.prevBoard[j][i]):
+					return j
+				j += 1
+			i += 1
+		return -1
