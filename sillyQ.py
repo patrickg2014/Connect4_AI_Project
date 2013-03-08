394fbadffe7e4c3e977ee8231005b012a70631a8
Quinn Thorsnes <quinn.frogman@gmail.com>
2013-03-07 21:46:59 -0800

    Win Checking added

--- sillyQ.py
+++ sillyQ.py
@@ -26,3 +26,83 @@ class sampleAI(AIClass.AI):
 				j += 1
 			i += 1
 		return -1
+	def findWinSpots(self,board):
+		checkResult = None
+		for column in range (6):
+			for row in range(5):
+				if board[column][row] != '_':
+					checkResult = triSpots(board,board[column][row], 'up',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'upRight',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'right',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'downRight',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'down',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'downLeft',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'left',column,row)
+					if checkResult != None:
+						return checkResult
+					checkResult = triSpots(board,board[column][row],'upLeft',column,row)
+					if checkResult != None:
+						return checkResult
+		return checkResult	
+	
+	def triSpots(self,board,pos,direction,x,y):
+		xMod = 0
+		yMod = 0
+		distance = 1
+		checked = False
+		riskFound = False
+		risk = None
+		if direction == 'up':
+			yMod = -1
+		elif direction == 'upRight':
+			yMod = -1
+			xMod = 1
+		elif direction == 'right':
+			xMod = 1
+		elif direction == 'downRight':
+			yMod = 1
+			xMod = 1
+		elif direction == 'down':
+			yMod = 1
+		elif direction == 'downLeft':
+			yMod = 1
+			xMod = -1
+		elif direction == 'left':
+			xMod = -1
+		elif direction == 'upLeft':
+			yMod = -1
+			xMod = -1
+		while not checked:
+			if y+(yMod*distance) < 0:
+				checked = True
+			if x+(xMod*distance) < 0:
+				checked = True
+			if x+(xMod*distance) > 6:
+				checked = True
+			if y+(yMod*distance) > 5:
+				checked = True
+			elif board[x+(xMod*distance)][y+(yMod*distance)] != piece:
+				checked = True
+			elif board[x+(xMod*distance)][y+(yMod*distance)] == piece:
+				distance+=1
+				if distance == 3:
+					riskFound = True
+					checked = True
+		if winnerFound == True:
+			if piece == 'x':
+				risk = 'computer'
+			elif piece == 'o':
+				risk = 'player'
+		return risk					
